01f0ddf0e53fd2d68042e3d617e27c565f8a63d8
arushan2 <rockarush2@gmail.com>
2024-07-24 10:38:06 +0530

    make some improvement

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
 
-function App() {
+const drawerWidth = 240;
+const navItems = ['Home', 'About', 'Resume', 'Services', 'Skills', 'Contact'];
+
+const FullScreenDrawer = styled(Drawer)(({ theme }) => ({
+  '& .MuiDrawer-paper': {
+    position: 'fixed',
+    width: '100%',
+    height: '100%',
+    bottom: 0,
+    left: 0,
+    transform: 'none',
+  },
+}));
+
+export default function DrawerAppBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        ARUSHAN
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+              <ListItemText primary={item} sx={{fontSize: '40'}}/>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav" sx={{ backgroundColor: 'black' }}>
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, }}
+          >
+            ARUSHAN
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' }, ml: 'auto' }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ ml: 'auto', display: { sm: 'none' } }}
+          >
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <MenuIcon />
+              <Typography sx={{ ml: 1 }}>MENUE</Typography>
+            </Box>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <FullScreenDrawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: '100%', height: '100%' },
+          }}
+          anchor="down"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {drawer}
+        </FullScreenDrawer>
+      </nav>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+        {/* Your main content goes here */}
+      </Box>
+    </Box>
   );
 }
-
-export default App;
